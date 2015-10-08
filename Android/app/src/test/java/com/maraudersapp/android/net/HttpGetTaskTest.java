23be8eb9acc31e1347f622cfd3c398bfff9d9668
Michael Maurer <mjmaurer7@gatech.edu>
2015-10-08 17:43:29 -0400

    this is a branch

---
+++ Android/app/src/test/java/com/maraudersapp/android/net/HttpGetTaskTest.java
@@ -0,0 +1,22 @@
+package com.maraudersapp.android.net;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Michael on 10/8/2015.
+ */
+public class HttpGetTaskTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
