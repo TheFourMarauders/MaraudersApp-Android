fa18a4fad702e9fe655d670675c134cc0c81f7d8
Joe <jrossi6@gatech.edu>
2015-10-17 00:55:12 -0400

    server comm factory with defaults

--- Android/app/src/main/java/com/maraudersapp/android/remote/ServerCommFactory.java
+++ Android/app/src/main/java/com/maraudersapp/android/remote/ServerCommFactory.java
@@ -1,7 +1,13 @@
 package com.maraudersapp.android.remote;
 
+import android.content.Context;
+
 /**
  * Created by Matthew on 10/16/2015.
  */
 public class ServerCommFactory {
+
+    public ServerComm build(Context context) {
+        return new HttpVolleyDispatcher(context, new ServerConfig("http://maraudersapp.cloudapp.net", 27380));
+    }
 }
